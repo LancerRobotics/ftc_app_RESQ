22f1645dbf16e1b0067fa48b849e9530c950782e
Sporky1414 <sporky1414@gmail.com>
2016-01-08 15:40:53 -0500

    Fixed more Git errors. We should probably look for a different way of storing code

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/ftclancers/DigitalLimitSwitch.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/ftclancers/DigitalLimitSwitch.java
@@ -2,7 +2,6 @@ package com.qualcomm.ftcrobotcontroller.opmodes.ftclancers;
 
 import com.qualcomm.ftcrobotcontroller.Keys;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.DigitalChannelController;
 
@@ -10,28 +9,14 @@ import com.qualcomm.robotcore.hardware.DigitalChannelController;
  * Created on 1/4/2016.
  */
 public class DigitalLimitSwitch extends OpMode {
-    DcMotor fr, fl;
     DigitalChannel limit1;
 
     @Override
     public void init() {
         limit1 = hardwareMap.digitalChannel.get(Keys.LIMIT_ONE);
         limit1.setMode(DigitalChannelController.Mode.OUTPUT);
-        fr = hardwareMap.dcMotor.get(Keys.frontRight);
-        fl = hardwareMap.dcMotor.get(Keys.frontLeft);
-        fr.setDirection(DcMotor.Direction.REVERSE);
-        limit1 = hardwareMap.digitalChannel.get(Keys.LIMIT_ONE);
-        limit1.setMode(DigitalChannelController.Mode.INPUT);
     }
     public void loop() {
         telemetry.addData("Limit switch is pressed: ", limit1.getState());
-    }
-    public void turn (double power) {
-        fl.setPower(power);
-        fr.setPower(-power);
-    }
-    public void rest() {
-        fr.setPower(0);
-        fl.setPower(0);
     }
 }
