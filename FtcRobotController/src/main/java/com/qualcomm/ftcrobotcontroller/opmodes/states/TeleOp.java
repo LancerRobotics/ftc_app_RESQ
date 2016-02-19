88b0473410ed107af1ede9ebaa98508be3056e9f
Sporky1414 <sporky1414@gmail.com>
2016-02-18 22:46:08 -0500

    Update TeleOp.java

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/states/TeleOp.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/states/TeleOp.java
@@ -209,6 +209,8 @@ public class TeleOp extends OpMode{
             rightTrigger = false;
             leftTrigger = false;
         }
+
+        //Auto-Trigger
         if(!gamepad2.x && !gamepad2.b && !gamepad2.a && !rightTrigger && !leftTrigger) {
             if (navx_device.getPitch() > 15) {
                 triggerLeft.setPosition(Keys.LT_TRIGGER);
