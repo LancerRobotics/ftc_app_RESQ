9a33a9b057eaecc75d9e9f82b5103f4ea2b6784a
mattquan <quan.matthew1999@gmail.com>
2016-04-14 00:54:20 -0400

    changed param in smoothmovevol2 to double. added camera stuff that was missing

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/worlds/AutonomousTemplate.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/worlds/AutonomousTemplate.java
@@ -1,16 +1,26 @@
 package com.qualcomm.ftcrobotcontroller.opmodes.worlds;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.hardware.Camera;
+import android.util.Log;
 
 import com.kauailabs.navx.ftc.AHRS;
 import com.kauailabs.navx.ftc.navXPIDController;
+import com.qualcomm.ftcrobotcontroller.Beacon;
 import com.qualcomm.ftcrobotcontroller.CameraPreview;
+import com.qualcomm.ftcrobotcontroller.FtcRobotControllerActivity;
 import com.qualcomm.ftcrobotcontroller.Keys;
+import com.qualcomm.ftcrobotcontroller.Vision;
+import com.qualcomm.ftcrobotcontroller.VisionProcess;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.AnalogInput;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
+
+import java.io.File;
 
 /**
  * Created by Matt on 3/26/2016.
@@ -65,10 +75,34 @@ public class AutonomousTemplate extends LinearOpMode {
         //telemetry.addData("Start Autonomous?", "Yes");
         waitForStart();
         smoothMoveVol2(48,false);
+        mCamera = ((FtcRobotControllerActivity) hardwareMap.appContext).mCamera;
+        //i need to init the camera and also get the instance of the camera        //on pic take protocol
+        telemetry.addData("camera","initingcameraPreview");
+        ((FtcRobotControllerActivity) hardwareMap.appContext).initCameraPreview(mCamera, this);
+
+        //wait, because I have handler wait three seconds b4 it'll take a picture, in initCamera
+        sleep(Vision.RETRIEVE_FILE_TIME);
+        //now we are going to retreive the image and convert it to bitmap
+        SharedPreferences prefs = hardwareMap.appContext.getApplicationContext().getSharedPreferences(
+                "com.quan.companion", Context.MODE_PRIVATE);
+        String path = prefs.getString(Keys.pictureImagePathSharedPrefsKeys, "No path found");
+        Log.e("path", path);
+        telemetry.addData("image",path);
+        //debug stuff - telemetry.addData("camera", "path: " + path);
+        File imgFile = new File(path);
+        image = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+        Log.e("image",image.toString());
+        //cool now u have the image file u just took the picture of
+        VisionProcess mVP = new VisionProcess(image);
+        Log.e("starting output","start");
+        telemetry.addData("starting output","doing smart computer stuff now");
+        Beacon beacon = mVP.output(hardwareMap.appContext);
+        Log.e("beacon",beacon.toString());
+        telemetry.addData("beacon",beacon);
 
     }
 
-    public void smoothMoveVol2 (int inches, boolean backwards) {
+    public void smoothMoveVol2 (double inches, boolean backwards) {
         //works best for at least 1000 ticks = 11.2 inches approx
         double rotations = inches / (Keys.WHEEL_DIAMETER * Math.PI);
         double totalTicks = rotations * 1120 * 3 / 2;
